be81984f4076a306b1e3ad506065a5d98f54e9e9
Quang Luong <quanglddev@gmail.com>
2021-08-18 20:29:45 -0400

    test run

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import time
 
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -25,33 +26,47 @@ def main():
     chrome_options.add_argument("--disable-gpu")
     driver = webdriver.Chrome(options=chrome_options)
     # driver = webdriver.Chrome("./assets/chromedriver")
+
+    # For debuggin
+    df = pd.DataFrame()
+    is_3_bar_play_list: list[bool] = []
+    has_acceptable_spread_list: list[bool] = []
+
     tickers = []
     qualified_tickers = []
 
     tickers += get_all_tickers_from_all_market_caps()
     tickers += get_benzinga_gappers(driver)
-
-    print(len(tickers))
+    df["Ticker"] = pd.Series(tickers)
 
     for idx, ticker in enumerate(tickers):
         try:
             tickerSymbol = ticker.upper()
             print(f"{idx + 1}: {tickerSymbol}")
             ohlcv = get_candlestick_data_for_ticker(tickerSymbol)
-            if check_if_is_3_bar_play(ohlcv) and check_if_has_acceptable_spreads(
-                tickerSymbol
-            ):
+            is_3_bar_play_list.append(check_if_is_3_bar_play(ohlcv))
+            has_acceptable_spread_list.append(
+                check_if_has_acceptable_spreads(tickerSymbol)
+            )
+            if is_3_bar_play_list[-1] and has_acceptable_spread_list[-1]:
                 qualified_tickers.append(tickerSymbol)
             time.sleep(1.5)
             break
         except Exception:
+            print("error")
             break
 
+    new_debug_file_name = f"{get_date_before_current().strftime('%Y-%m-%d_debug')}.txt"
+    new_debug_file_path = os.path.join("out", new_debug_file_name)
     new_file_name = f"{get_date_before_current().strftime('%Y-%m-%d')}.txt"
     new_file_path = os.path.join("out", new_file_name)
     with open(new_file_path, "w+") as f:
         for qualified_ticker in qualified_tickers:
             f.write(f"{qualified_ticker}\n")
+
+    df["Is 3/4 bar play"] = pd.Series(is_3_bar_play_list)
+    df["Has acceptable spreads"] = pd.Series(has_acceptable_spread_list)
+    df.to_csv(new_debug_file_path, index=False)
 
 
 main()
